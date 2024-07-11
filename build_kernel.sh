554c52dd699a3ad6423507e8272564996c5aa052
hhihhio <netbat4399@gmail.com>
2024-07-11 07:07:06 +0700

    update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,8 +4,11 @@ export ARCH=arm
 export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 mkdir output
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_j5xlte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+make -C $(pwd) O=output clean && make -C $(pwd) O=output mrproper
+make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_j5xlte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig TIMA_DEFCONFIG=tima_defconfig
+#make -C $(pwd) O=output menuconfig
+make -C $(pwd) O=output -j$(nproc --all)
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+tools/dtbTool -s 2048 -o output/arch/arm/boot/dt.img -p output/scripts/dtc/ output/arch/arm/boot/dts/ -v
+#cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
 
